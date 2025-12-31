01b2039f6e0fbb9aa1b47774d8f33f73697f4937
trumgbergh <68943610+trumgbergh@users.noreply.github.com>
2025-12-31 12:32:36 +1000

    update

--- cf.py
+++ cf.py
@@ -1,44 +1,74 @@
+import pprint
+import random
+from typing import Optional, Sequence
+
 import requests
-from typing import Optional
-from typing import Sequence
 
 user = "Voltair"
 problem = set()
-cntp = 0;
-CFAPI = "https://codeforces.com/api/"
+CFAPI = "https://codeforces.com/api"
 
 
-def LogIn(name):
-    user = name;
-
-def Greetings()
+def greetings():
     print("Hi ", user, "\n")
-    print(
+
 
 def space(cnt):
     return " " * cnt
 
-def getSolved():
-    r = requests.get("https://codeforces.com/api/user.status?handle=" + user + "&from=1")
-    f = r.json();
+
+def get_solved():
+    r = requests.get(f"{CFAPI}/user.status?handle=" + user + "&from=1")
+    f = r.json()
+    cntp = 0
     for sub in f["result"]:
         cntp += 1
         if sub["verdict"] == "OK":
             s = str(sub["contestId"]) + str(sub["problem"]["index"])
             problem.add(s)
-    print(len(problem))
+    return problem
 
-def getSubmission(cnt):
+
+def get_submission(cnt):
     s = str(cnt)
     print("Last " + s + " submission(s):\n")
-    r = requests.get("https://codeforces.com/api/user.status?handle=" + user + "&from=1&count=" + s)
-    f = r.json();
-    print(f)
-    headers = ["Problem", "Name", "Rating", "Verdict"]
+    r = requests.get(f"{CFAPI}/user.status?handle=" + user + "&from=1&count=" + s)
+    f = r.json()
+    if f["status"] != "OK":
+        print("Failed to get submission data saj")
+        return
+
+    for submission in f["result"]:
+        idx = submission["problem"]["index"]
+        name = submission["problem"]["name"]
+        verdict = submission["verdict"]
+        if verdict == "OK":
+            print(f"{idx}. {name}   |    Verdict: {verdict}")
+        else:
+            test_passed = submission["passedTestCount"]
+            print(
+                f"{idx}. {name}   |    Verdict: {verdict}, Test passed: {test_passed}"
+            )
+
+
+def get_random_problem(elo_range: tuple[int, int] = (1500, 1900)):
+    r = requests.get(f"{CFAPI}/problemset.problem")
+    f = r.json()
+    solved_problems = get_solved()
+    print(solved_problems)
+    if f["status"] != "OK":
+        return
+    all_problems = f["result"]["problems"]
+    # valid_problems = []
+    # for p in all_problems:
+
+    # problem = random.choice(valid_problems)
+
 
 def main(agrv: Optional[Sequence[str]] = None) -> int:
-    getSubmission(1)
-    getSolved()
+    # getSubmission(2)
+    getRandomProblem()
+
 
 if __name__ == "__main__":
     exit(main())
---
+++ main.py
@@ -0,0 +1,75 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import json
+from contextlib import contextmanager
+from typing import Generator
+import pprint
+import argparse
+
+class CompetitiveCompanionHandler(BaseHTTPRequestHandler):
+    callback = None
+
+    def __init__(self, request, client_address, server):
+        super().__init__(request, client_address, server)
+
+    @staticmethod
+    def set_callback(callback):
+        callback = callback
+
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length'])
+        payload = self.rfile.read(content_length).decode('utf-8')
+        data = json.loads(payload)
+
+        if CompetitiveCompanionHandler.callback:
+            CompetitiveCompanionHandler.callback(data)
+
+@contextmanager
+def run_server(
+        serverclass = HTTPServer,
+        handler_class = CompetitiveCompanionHandler,
+        port = 10046,
+        callback = None,
+) -> Generator[HTTPServer, None, None]:
+    server_address = ("0.0.0.0", port)
+    httpd = serverclass(server_address, handler_class)
+
+    handler = handler_class
+    handler.callback = callback
+
+    print(f'Starting server on port {port}...')
+    try:
+        yield httpd
+    finally:
+        httpd.server_close()
+        print(f'Stopping server on port {port}.')
+
+
+def listen_once():
+    payload = None
+
+    def set_payload(data) -> None:
+        nonlocal payload
+        payload = data
+
+    with run_server(callback = set_payload) as server:
+        server.timeout = None
+        server.handle_request()
+
+    pprint.pprint(payload)
+    if not isinstance(payload, dict):
+        return
+
+    for i, test in enumerate(payload["tests"]):
+        test_id = i + 1
+        with open(f"inp{test_id}", mode="w", encoding="utf-8") as f:
+            f.write(test["input"])
+        with open(f"out{test_id}", mode="w", encoding="utf-8") as f:
+            f.write(test["output"])
+
+
+def main(argv: Sequence[str] | None = None) -> int:
+    parser = argparse.ArgumentParser()
+    return 0
+
+if __name__ == "__main__":
+    raise SystemExit(main())
